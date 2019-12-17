890a9d48a18cefdb6acbf3ec4714af1b6bc7814f
okazimirov <Hc147852>
2019-12-17 20:13:37 +0200

    add salary to Person

--- src/ua/com/pointer/Person.java
+++ src/ua/com/pointer/Person.java
@@ -4,6 +4,7 @@ public class Person {
 
     public String firstName = "Sasha";
     public int age = 28;
+    public int salary = 1000;
 
     public void say(){
         System.out.println("Salary is 1000$");
