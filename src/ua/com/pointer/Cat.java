2ae2e231571524a991f672a3c75a8dc8ff3983d3
okazimirov <Hc147852>
2019-12-20 19:20:12 +0200

    age2

--- src/ua/com/pointer/Cat.java
+++ src/ua/com/pointer/Cat.java
@@ -3,7 +3,8 @@ package ua.com.pointer;
 public class Cat {
 
     String name = "NEW CAT";
-    String color = "BLACK";
+    String color = "BLAC";
     int age = 3;
     int age2 = 3;
+    int age3 = 3;
 }
