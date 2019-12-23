b1923ac5607a3c4412bf542137a0607007262b7f
kacheniuk <59092380+kacheniuk@users.noreply.github.com>
2019-12-23 21:19:03 +0200

    add Elephant

---
+++ src/ua/com/pointer/Main.java
@@ -0,0 +1,14 @@
+package ua.com.pointer;
+
+import ua.com.pointer.Elephant;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Elephant Clara= new Elephant("Clara", 24, 24,"RED");
+
+        System.out.println(Clara);    }
+}
+
+
