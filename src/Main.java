e5899251c8fdab9aaabe967ef76bf183c26184c3
Vitalii Solodovnyk <vitaliysecretua@gmail.com>
2019-12-23 11:17:40 +0200

    add some characteristics

--- src/Main.java
+++ src/Main.java
@@ -3,10 +3,20 @@ import ua.com.pointer.Robot;
 public class Main {
 
     public static void main(String[] args) {
+        Robot myRobot1 = new Robot();
+        myRobot1.name = "Bender";
+        myRobot1.metal = "Iron";
+        myRobot1.color = "Gold";
+        myRobot1.age = 50;
 
-        Robot person = new Robot();
+        System.out.println("My name " + myRobot1.name + ", " + "i made of " + myRobot1.metal + ", " + "my color is " + myRobot1.color + ", " + "and my age " + myRobot1.age);
         
+        myRobot1.shooting();
+        myRobot1.speak();
+
+
+
+
     }
 }
-
 
