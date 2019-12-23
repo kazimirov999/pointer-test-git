e3c75862f23310320a4ad50ffb862bc2f6d2fdf9
kacheniuk <59092380+kacheniuk@users.noreply.github.com>
2019-12-23 21:23:21 +0200

    add Elephant

---
+++ src/ua/com/pointer/Elephant.java
@@ -0,0 +1,15 @@
+package ua.com.pointer;
+
+public class Elephant {
+
+    public String Name = "David";
+    public int age = 15;
+    public int weight = 5000;
+    public String color = "Blue";
+    public Elephant(String name, int age, int weight, String color) {
+        Name = name;
+        this.age = age;
+        this.weight = weight;
+        this.color = color;
+    }
+}
